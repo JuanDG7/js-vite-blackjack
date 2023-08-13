15f0dc508ef2958fb5e8d4f66cf8247c4224e4a8
Juan Duarte <jmdmg86@gmail.com>
2023-08-13 04:37:46 -0400

    First commit

---
+++ src/blackjack/usecases/valor-carta.js
@@ -0,0 +1,10 @@
+/**
+ * Obtener el valor de la carta
+ * @param {string} carta ej=10
+ * @returns {Number} valor de la carta
+ */
+
+export const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
